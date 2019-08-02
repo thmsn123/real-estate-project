b714a6c59a24bee1303ec566316c15e142eade84
Toma Tomov <ttomov@shopmetrics.com>
2019-08-02 17:58:21 +0300

    remove vuefire

--- src/main.js
+++ src/main.js
@@ -6,18 +6,6 @@ import Vuelidate from 'vuelidate'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import FireBase from "firebase";
-
-const config = {
-  apiKey: "AIzaSyDHidM-Bk0JA3eWcQ2M8iyXK4wqUWGHwmA",
-  authDomain: "localhost",
-  databaseURL: "https://real-estate-project-e32ed.firebaseio.com/",
-  projectId: "real-estate-project-e32ed",
-  storageBucket: "gs://real-estate-project-e32ed.appspot.com/",
-  messagingSenderId: "833417375005"
-};
-
-FireBase.initializeApp(config);
 
 Vue.use(Vuelidate);
 Vue.use(BootstrapVue);
